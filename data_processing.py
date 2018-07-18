bf999f1c88fd5cd58422d0a9dd8e5820da305c02
Unknown <no.mercy.cs573@gmail.com>
2018-07-17 23:30:26 -0500

    better processing data
    .

--- data_processing.py
+++ data_processing.py
@@ -3,59 +3,51 @@ import pandas as pd
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("daily_MSFT.csv")
+input_file = "daily_MSFT.csv"
+data = pd.read_csv(input_file)
+#print(data.shape)
+#print(data.head())
 
-data = data.sort_values('timestamp')
+def show_graphic(data_frame):
+        plt.figure(figsize=(14,7))
+        plt.plot(range(data.shape[0]),(data['low']+data['high'])/2.0)
+        plt.xticks(range(0,data.shape[0],500),data['timestamp'].loc[::500],rotation=45)
+        plt.xlabel('fecha',fontsize=18)
+        plt.ylabel('precio medio',fontsize=18)
+        plt.show()
+#data = data.sort_values('timestamp')
+#show_graphic(data)
 
-def show_data(data):
-	plt.figure(figsize = (18,9))
-	plt.plot(range(data.shape[0]),(data['low']+data['high'])/2.0)
-	plt.xticks(range(0,data.shape[0],500),data['timestamp'].loc[::500],rotation=45)
-	plt.xlabel('Date',fontsize=18)
-	plt.ylabel('Mid Price',fontsize=18)
-	plt.show()
-
-
-
-
-
-data.drop(['timestamp'],1,inplace=True)
-#Nonmalizando la data
-scaler = preprocessing.MinMaxScaler()
-data['open'] = scaler.fit_transform(data.open.values.reshape(-1,1))
-data['high'] = scaler.fit_transform(data.high.values.reshape(-1,1))
-data['low'] = scaler.fit_transform(data.low.values.reshape(-1,1))
-data['close'] = scaler.fit_transform(data.close.values.reshape(-1,1))
-data['volume'] = data['volume'].astype(float)
-data['volume'] = scaler.fit_transform(data.volume.values.reshape(-1,1))
-data_volume_temp = data['volume']
-data['volume'] = data['close']
-data['close'] = data_volume_temp
+def normalize(data):
+        scaler = preprocessing.MinMaxScaler()
+        data['open'] = scaler.fit_transform(data.open.values.reshape(-1,1))
+        data['high'] = scaler.fit_transform(data.high.values.reshape(-1,1))
+        data['low'] = scaler.fit_transform(data.low.values.reshape(-1,1))
+        data['close'] = scaler.fit_transform(data.close.values.reshape(-1,1))
+        data['volume'] = data['volume'].astype(float)
+        data['volume'] = scaler.fit_transform(data.volume.values.reshape(-1,1))
+        data['closef'] = data.close
+        data.drop(['close'],1,inplace=True)
+        return data
 
 
-
-
-#Tratamos la data
-def load_data(bolsa, tamanio_sequencia):
-	n_caracteristicas = len(bolsa.columns)
-	data = bolsa.as_matrix()
-	resultados = []
-
-	for indece in range(len(data) - (tamanio_sequencia + 1)):
-		resultados.append(data[indece: indece + tamanio_sequencia + 1])
-
-	resultados = np.array(resultados)
-	fila = resultados.shape[0]
-
-	entrenamiento = resultados[:int(fila),:]
-
-	data_X= entrenamiento[:, :-1]
-	data_y = entrenamiento[:, -1][:, -1]
-
-	data_X = np.reshape(data_X,(data_X.shape[0],data_X.shape[1],n_caracteristicas))
-
-	return [data_X,data_y]
-
-print(len(load_data(data,30)[0][0]))
+#Tratamiento de la data
+def procesamiento_data(bolsa, numero_dias):
+    n_caracteristicas = len(bolsa.columns)
+    data = bolsa.as_matrix()
+    #print(data)
+    entrenamiento = []
+    n = len(data)
+    for indice in range(n - numero_dias):
+        entrenamiento.append(data[indice:indice+numero_dias+1])
+    entrenamiento = np.array(entrenamiento)
+    X = entrenamiento[:,:-1]
+    y = entrenamiento[:,-1][:,-1]
+    #X = np.reshape(X,(X.shape[0],X.shape[1],n_caracteristicas))
+    return [X,y]
+	
+'''data.drop(['timestamp'],1,inplace=True) #no se si deberiamos dropear la fecha men
+normalize(data)
+print(procesamiento_data(data,30)[1][1])'''
 
 
